580a0510c4b3be82eb3122c9b5ce32914728ada7
Kohei Suzuki <eagletmt@gmail.com>
2014-05-05 14:40:42 +0900

    Add my timeline page

--- app/src/main/java/cc/wanko/karin/app/adapters/MainPagerAdapter.java
+++ app/src/main/java/cc/wanko/karin/app/adapters/MainPagerAdapter.java
@@ -5,6 +5,7 @@ import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentPagerAdapter;
 
 import cc.wanko.karin.app.client.HomeTimelineSource;
+import cc.wanko.karin.app.client.UserStatusSource;
 import cc.wanko.karin.app.fragments.StatusListFragment;
 import cc.wanko.karin.app.fragments.UserListListFragment;
 
@@ -22,24 +23,31 @@ public class MainPagerAdapter extends FragmentPagerAdapter {
     @Override
     public Fragment getItem(int position) {
         switch (position) {
-            case 0:
+            case 0: {
                 StatusListFragment fragment = new StatusListFragment();
                 fragment.setStatusSource(new HomeTimelineSource(activity));
                 return fragment;
+            }
             case 1:
                 return new UserListListFragment();
+            case 2: {
+                StatusListFragment fragment = new StatusListFragment();
+                fragment.setStatusSource(new UserStatusSource(activity, -1));
+                return fragment;
+            }
         }
         throw new IllegalStateException("not reached");
     }
 
     @Override
     public int getCount() {
-        return 2;
+        return 3;
     }
 
     private static final String[] PAGE_TITLES = {
             "Home Timeline",
             "Lists",
+            "Me",
     };
 
     @Override
